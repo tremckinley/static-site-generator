b5aeb912f678f50197891c81378e54322c72bc90
Tremckinley <tremckinley901@gmail.com>
2025-05-13 20:41:08 -0500

    caught up to lesson

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from textnode import TextNode, TextType
+from textnode import TextNode, TextType, text_node_to_html_node
 
 class TestTextNode(unittest.TestCase):
     def test_eq(self):
@@ -17,6 +17,29 @@ class TestTextNode(unittest.TestCase):
         node = TextNode("This is a text node", TextType.CODE, "www.crooked.com")
         node2 = TextNode("This is another text node", TextType.CODE, "www.foxnews.com")
         self.assertEqual(node.text_type.value, node2.text_type.value)
+
+class TestTextNodeToHTMLNode(unittest.TestCase):
+    def test_text(self):
+        node = TextNode("This is a text node", TextType.NORMAL)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node['tag'], None)
+        self.assertEqual(html_node['value'], "This is a text node")
+
+    def test_image(self):
+        node = TextNode("This is an image", TextType.IMAGE, "https://www.boot.dev")
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node["tag"], "img")
+        self.assertEqual(html_node['value'], "")
+        self.assertEqual(
+            html_node['props'],
+            {"src": "https://www.boot.dev", "alt": "This is an image"},
+        )
+
+    def test_bold(self):
+        node = TextNode("This is bold", TextType.BOLD)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node['tag'], "b")
+        self.assertEqual(html_node['value'], "This is bold")
 
 if __name__ == "__main__":
     unittest.main()
--- src/textnode.py
+++ src/textnode.py
@@ -23,3 +23,27 @@ class TextNode:
 
     def __repr__(self):
         return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
+    
+def text_node_to_html_node(text_node):
+    if text_node.text_type == TextType.NORMAL:
+        return {"tag": None, "value": text_node.text}
+    elif text_node.text_type == TextType.BOLD:
+        return {"tag": "b", "value": text_node.text}
+    elif text_node.text_type == TextType.ITALIC:
+        return {"tag": "i", "value": text_node.text}
+    elif text_node.text_type == TextType.CODE:
+        return {"tag": "code", "value": text_node.text}
+    elif text_node.text_type == TextType.LINK:
+        return {
+            "tag": "a",
+            "props": {"href": text_node.url},
+            "value": text_node.text,
+        }
+    elif text_node.text_type == TextType.IMAGE:
+        return {
+            "tag": "img",
+            "props": {"src": text_node.url, "alt": text_node.text},
+            "value": "",
+        }
+    else:
+        raise ValueError(f"Unknown text type: {text_node.text_type}")
